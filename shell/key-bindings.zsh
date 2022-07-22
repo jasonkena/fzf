07e4c6d61a34f35e26ad222d4e0efe1501a16a5b
Jason Adhinarta <jasonice6@gmail.com>
2022-07-21 23:50:58 -0400

    Allow arguments to be passed to aliased commands

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -95,17 +95,25 @@ bindkey -M viins '\ec' fzf-cd-widget
 
 # CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
-  local selected num fc_history query
+  local selected num fc_history query tag rest
   setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
   fc_history=$(fc -rl 1 | awk '{ cmd=$0; sub(/^\s*[0-9]+\**\s+/, "", cmd); if (!seen[cmd]++) print $0 }')
   if [ -n "$LBUFFER" ]; then
-    # find lines that match the query, trimp whitespace, then extract the ids
-    local results=$(grep -P "(^|\s)#$LBUFFER($|\s)" <<< "$fc_history" | awk '{$1=$1};1' | grep -oP "^\d+")
+    # get first word
+    tag=$(perl -lane 'print "@F[0]"' <<<"$LBUFFER")
+    # everything else
+    rest=$(perl -lane 'print "@F[1..$#F]"' <<<"$LBUFFER")
+    # find lines that match the query, trim whitespace, then extract the ids
+    local results=$(grep -P "(^|\s)#$tag($|\s)" <<< "$fc_history" | awk '{$1=$1};1' | grep -oP "^\d+")
 
     # check if the number of lines in results is equal to one
     if [ -n "$results" ]; then
       if [[ "$results" =~ '^[0-9]+$' ]]; then
         zle vi-fetch-history -n $results
+        # strip everything after first tag, then strip whitespace
+        LBUFFER=$(perl -ne "s/(^|\s)#\w+($|\s).*//g; print;" <<<"$LBUFFER" | awk '{$1=$1};1')
+        # add arguments
+        LBUFFER+=" $rest"
         zle reset-prompt
         zle accept-line
         return 0
